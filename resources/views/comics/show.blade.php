f200577527b570d32bd8d64f1fe8f656dae41f36
Lorenzo Ariatta <lorenzotk77@gmail.com>
2022-05-25 17:42:35 +0200

    layout parts

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,4 +1,22 @@
 @extends('layout')
 
 @section('show')
+    <div class="container-detail">
+        <div class="comic-detail">
+            <h1>{{ $comic->title }}</h1>
+            <div class="comic-description">
+                <img src="{{ $comic->thumb }}" alt="{{ $comic->title }}">
+                <p>{{ $comic->description }}</p>
+            </div>
+            <div class="price">
+                <h3>Price: </h3>
+                <h2>$ {{ $comic->price }}</h2>
+            </div>
+            <div class="comic-info">
+                <h5>Series: {{ $comic->series }}</h5>
+                <h5>On sale: {{ $comic->sale_date }}</h5>
+                <h5>Type: {{ $comic->type }}</h5>
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,16 +6,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="/css/app.css">
-    <title>Document</title>
+    <title>DC Comics</title>
 </head>
 
 <body>
 
-    @yield('buttonGo')
+    <header>
+        <div class="title1">
+            <p>DC</p>
+        </div>
+        <div class="title2">
+            Comics
+        </div>
+    </header>
 
-    @yield('index')
+    <main>
+        @yield('buttonGo')
 
-    @yield('show')
+        @yield('index')
+
+        @yield('show')
+    </main>
 
 </body>
 

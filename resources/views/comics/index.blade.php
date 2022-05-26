40ca511b06ee0418bd9b95780cfd23c46ce7fede
Lorenzo Ariatta <lorenzotk77@gmail.com>
2022-05-26 16:49:51 +0200

    fix buttons

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -8,7 +8,7 @@
                 <h3>{{ $comic->title }}</h3>
                 <h4>Price: $ {{ $comic->price }}</h4>
                 <h4>On sale: {{ $comic->sale_date }}</h4>
-                <a href="{{ route('comics.show', $comic->id) }}"><button>SHOW DETAILS</button></a>
+                <a href="{{ route('comics.show', $comic->id) }}"><button class="detail-button">SHOW DETAILS</button></a>
             </div>
         @endforeach
     </div>
